828a761f68620f292ba142cb0687d2a367106609
CryoFlar3 <ndukes@computermentors.org>
2018-06-29 10:12:20 -0400

    Initial commit

---
+++ src/org/computermentors/NyjelDukes/Main.java
@@ -0,0 +1,12 @@
+package org.computermentors.NyjelDukes;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("We are creating a new PEZ! ");
+
+        PezDispenser dispenser = new PezDispenser("Finn");
+        System.out.println(dispenser.characterName );
+    }
+}
---
+++ src/org/computermentors/NyjelDukes/PezDispenser.java
@@ -0,0 +1,9 @@
+package org.computermentors.NyjelDukes;
+
+public class PezDispenser {
+    public String characterName;
+
+    public PezDispenser(String characterName){
+        this.characterName = characterName;
+    }
+}
